c20f7427a1d085d7c799e46d527d4236340ffa50
Artur Durasiewicz <artur@durasiewi.cz>
2024-06-24 20:35:07 +0200

    Token type enum.

--- FFalse/Program.fs
+++ FFalse/Program.fs
@@ -1,2 +1,44 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿[<Literal>]
+let Pick = 'ø';
+
+type TokenType =
+    | OpenSquareBracket
+    | CloseSquareBracket
+    | Identifier
+    | Number
+    | Literal
+    | Colon
+    | Semicolon
+    | Plus
+    | Minus
+    | Asterisk
+    | Slash
+    | Equals
+    | GreaterThan
+    | Ampersand
+    | Bar
+    | Dollar
+    | Percent
+    | Backslash
+    | At
+    | Question
+    | Exclamation
+    | Tilde
+    | Dot
+    | Comma
+    | Underscore
+    | Hash
+    | Caret
+    | Pick
+    | Section
+
+let lex code =
+    let rec doLex code tokens =
+        match code with
+        | head :: tail ->
+            printfn $"%c{head} "                
+            doLex tail tokens
+        | [] -> ()
+    doLex (List.ofSeq code)
+    
+lex "{  }" |> ignore
