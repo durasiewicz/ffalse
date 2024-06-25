dcf879306f6273d1f2a5500798af501303e6c275
Artur Durasiewicz <artur@durasiewi.cz>
2024-06-25 11:54:52 +0200

    Lexer.

--- FFalse/Program.fs
+++ FFalse/Program.fs
@@ -1,12 +1,14 @@
-﻿[<Literal>]
+﻿open System
+
+[<Literal>]
 let PickCharacter = 'ø';
 
 type TokenType =
     | OpenSquareBracket
     | CloseSquareBracket
-    | Identifier
-    | Number
-    | Literal
+    | Identifier of value : string
+    | Number of value : int
+    | Literal of value : string
     | Colon
     | Semicolon
     | Plus
@@ -63,12 +65,41 @@ let parseCharacter character =
     | _ -> None
    
 let lex code =
+    let rec scanNumber code (number : string) =
+        match code with
+        | h :: t when Char.IsDigit(h) -> scanNumber t (number + string h)
+        | _ -> (code, int number)
+        
+    let rec scanIdentifier code identifier =
+        match code with
+        | h :: t when Char.IsLetter(h) -> scanIdentifier t (identifier + string h)
+        | _ -> (code, identifier)
+        
+    let rec scanLiteral code literal =
+        match code with
+        | h :: t when h <> '"' -> scanLiteral t (literal + string h)
+        | _ ->
+            match code with
+            | [] -> raise (InvalidOperationException("Found unclosed literal."))
+            | h :: t -> (t, literal)
+    
     let rec doLex code tokens =
         match code with
         | head :: tail -> 
             match parseCharacter head with
             | Some t -> doLex tail (t :: tokens)
-            | None -> doLex tail tokens            
+            | None ->
+                match head with
+                | h when Char.IsDigit(h) ->
+                    let code, number = scanNumber (head :: tail) ""
+                    doLex code (Number(number) :: tokens)
+                | h when Char.IsLetter(h) ->
+                    let code, identifier = scanIdentifier (head :: tail) ""
+                    doLex code (Identifier(identifier) :: tokens)
+                | h when h = '"' ->
+                    let code, literal = scanLiteral (tail) ""
+                    doLex code (Literal(literal) :: tokens)
+                | _ -> doLex tail tokens            
         | [] -> tokens
         
     let tokens = List.rev (doLex (List.ofSeq code) [])
@@ -76,4 +107,4 @@ let lex code =
     for element in tokens do
         printf "%A " element
     
-lex "=~][^]#%"
+lex "=~][^22\"ala ma kota\"]#%"
